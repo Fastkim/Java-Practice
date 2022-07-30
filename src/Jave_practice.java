794aed4679f5f054964fd234799b5bfe7db78268
DaHun kim <ekgns019@naver.com>
2022-07-31 01:21:15 +0900

    first commit

---
+++ src/Jave_practice.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Jave_practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		double d = 1.23456789;
+		System.out.printf("%14.6f\n",d);
+		
+		System.out.printf("[%s]\n","www.codechobo.com");
+		System.out.printf("[%20s]\n","www.codechobo.com");
+		System.out.printf("[%-20s]\n","www.codechobo.com");
+		System.out.printf("[%.10s]\n","www.codechobo.com");
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int num = scanner.nextInt(); 
+		int num2 = scanner.nextInt();
+		
+		System.out.println(num);
+		System.out.println(num2);
+		
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		String input = scanner.nextLine();
+		int num = Integer.parseInt(input);
+		
+		System.out.println(num);
+		*/
+		
+		
+		double pi = 3.141592;
+		
+		System.out.println(pi*1000);
+		System.out.println((int)(pi*1000));
+		System.out.println((int)(pi*1000)/1000);
+		System.out.println((int)(pi*1000)/1000.0);
+		
+		
+	}
+
+}
